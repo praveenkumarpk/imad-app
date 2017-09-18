f358662b5d221497e8f0e9af01d108b40c522126
praveenkumarpk <praveenkumar.parames@gmail.com>
2017-09-18 23:50:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/article1', function (req, res) {
 
 app.get('test1',function(req,res)
 {
-  res.sendFile(path.join(__dirname,'test')) ; 
+  res.sendFile(path.join(__dirname,'test.html')) ; 
 });
 
 
