076908dca8ecd889e6504733a7e05f2522b6c778
praveenkumarpk <praveenkumar.parames@gmail.com>
2017-09-18 23:48:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.send('eefref');
+});
 
 //praveenkumar test1.html
 
